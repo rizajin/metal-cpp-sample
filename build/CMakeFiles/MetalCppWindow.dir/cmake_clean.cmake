bfda60db23c69cbed3ddf9468b3882f34448c1b5
riza <jarkko.vaisanen@live.fi>
2025-04-20 22:57:22 +0300

    Apples sample by using CMake and metal-cpp

---
+++ build/CMakeFiles/MetalCppWindow.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MetalCppWindow.dir/main.cpp.o"
+  "CMakeFiles/MetalCppWindow.dir/main.cpp.o.d"
+  "MetalCppWindow"
+  "MetalCppWindow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MetalCppWindow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
